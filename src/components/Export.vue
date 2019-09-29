4b83bcad9251185dde12256d72cd5d4b6917681e
Steven Dostert <steven.dostert@icloud.com>
2019-09-29 22:43:08 +0200

    change name of exported file

--- src/components/Export.vue
+++ src/components/Export.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-btn :text="showText" :icon="!showText" :href="href" :download="`Rechnung-${id}.json`" @click.stop>
+  <v-btn :text="showText" :icon="!showText" :href="href" :download="`${name}.json`" @click.stop>
     <span v-if="showText"  class="mr-2 d-none d-sm-flex">Download</span>
     <v-icon v-if="showText">mdi-download</v-icon>
 	<v-tooltip v-else bottom>
@@ -20,15 +20,15 @@ export default {
 		showText: Boolean,
 		id: String,
 	},
-	data: () => ({
-	}),
 	computed: {
 		href() {
 			const file = new Blob([JSON.stringify(this.$store.state.bazaars[this.id])], { type: "application/json" });
 			return URL.createObjectURL(file);
+		},
+		name() {
+			return this.$store.state.bazaars[this.id].name;
 		}
 	},
-	methods: {}
 };
 </script>
 
